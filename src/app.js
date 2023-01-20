9c5bcbcdcd3e0159adb12c03e7400764bf5d3f37
Olga Gustafsson <olgagustafsson@MacBook-Air-som-tillhor-Olga.local>
2023-01-20 10:14:02 +0100

    update

--- src/app.js
+++ src/app.js
@@ -1,33 +1,25 @@
-
+"use strict";
 const url = "https://api.thecatapi.com/v1/images/search?limit=10";
-const catsDiv = document.querySelector("#img") as HTMLImageElement;
-const catsSec = document.querySelector(".a") as HTMLElement;
-
+const catsDiv = document.querySelector("#img");
+const catsSec = document.querySelector(".a");
 const cat = fetch(url)
     .then(response => {
-        return response.json();
-    })
+    return response.json();
+})
     .then(catsArray => {
-        printCat(catsArray);
-
-        console.log(catsArray);
-        
-    });
-
-function printCat(cats: any) {
-    let button = document.querySelector("#button") as HTMLButtonElement;
+    printCat(catsArray);
+    console.log(catsArray);
+});
+function printCat(cats) {
+    let button = document.querySelector("#button");
     button.addEventListener("click", (event) => {
         event.preventDefault();
-        
-        
         let randomN = Math.floor(Math.random() * cats.length + 1); // return slumpmässigt heltal 
         catsDiv.innerHTML = "";
         cats.src = cats[randomN].url;
         catsDiv.append(cats.src);
         catsSec.append(catsDiv);
-        
-       
         console.log(cats);
-
-    });     
-};
+    });
+}
+;
